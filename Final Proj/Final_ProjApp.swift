2b3945e56bcf5593cc7f21129f74cc07c5d73849
Rebecca Lee <rrrebeccalee@gmail.com>
2023-06-12 08:30:21 -0700

    Initial Commit

---
+++ Final Proj/Final_ProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjApp.swift
+//  Final Proj
+//
+//  Created by Rebecca Lee on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
